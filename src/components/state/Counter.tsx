ebf51cc29a8a8b6c0657bded5953282027eee4ba
aosante <aosantea@ucenfotec.ac.cr>
2021-12-19 23:28:50 -0600

    .

--- src/components/state/Counter.tsx
+++ src/components/state/Counter.tsx
@@ -4,22 +4,27 @@ type CounterState = {
   count: number;
 };
 
-type CounterAction = {
-  type: string;
+type UpdateAction = {
+  type: 'INCREMENT' | 'DECREMENT';
   payload: number;
 };
 
+type ResetAction = {
+  type: 'RESET';
+};
+
+type CounterAction = UpdateAction | ResetAction;
+
 const intialState = { count: 0 };
 
-const counterReducer = (
-  state: CounterState,
-  { type, payload }: CounterAction
-) => {
-  switch (type) {
+const counterReducer = (state: CounterState, action: CounterAction) => {
+  switch (action.type) {
     case 'INCREMENT':
-      return { count: state.count + payload };
+      return { count: state.count + action.payload };
     case 'DECREMENT':
-      return { count: state.count + payload };
+      return { count: state.count + action.payload };
+    case 'RESET':
+      return intialState;
     default:
       return state;
   }
@@ -32,13 +37,12 @@ export const Counter = () => {
     <>
       Count: {count}
       <button onClick={() => dispatch({ type: 'INCREMENT', payload: 10 })}>
-        {' '}
         Increment 10
       </button>
       <button onClick={() => dispatch({ type: 'DECREMENT', payload: 10 })}>
-        {' '}
         Decrement 10
       </button>
+      <button onClick={() => dispatch({ type: 'RESET' })}>Reset</button>
     </>
   );
 };
