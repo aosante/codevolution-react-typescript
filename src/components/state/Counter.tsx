085db63eb27e523a6cfb55d6956ddc9c30e30796
aosante <aosantea@ucenfotec.ac.cr>
2021-12-19 23:15:30 -0600

    .

---
+++ src/components/state/Counter.tsx
@@ -0,0 +1,43 @@
+import { useReducer } from 'react';
+
+type CounterState = {
+  count: number;
+};
+
+type CounterAction = {
+  type: string;
+  payload: number;
+};
+
+const intialState = { count: 0 };
+
+const counterReducer = (
+  state: CounterState,
+  { type, payload }: CounterAction
+) => {
+  switch (type) {
+    case 'INCREMENT':
+      return { count: state.count + payload };
+    case 'DECREMENT':
+      return { count: state.count + payload };
+    default:
+      return state;
+  }
+};
+
+export const Counter = () => {
+  const [{ count }, dispatch] = useReducer(counterReducer, intialState);
+  return (
+    <>
+      Count: {count}
+      <button onClick={() => dispatch({ type: 'INCREMENT', payload: 10 })}>
+        {' '}
+        Increment 10
+      </button>
+      <button onClick={() => dispatch({ type: 'DECREMENT', payload: 10 })}>
+        {' '}
+        Decrement 10
+      </button>
+    </>
+  );
+};
