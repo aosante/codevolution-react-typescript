1a5c654dacc94b79bb9cf39d557ee2f34c737a39
aosante <aosantea@ucenfotec.ac.cr>
2022-04-09 00:33:59 -0600

    checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
-import './App.css';
-import { Button } from './components/Button';
-import { Container } from './components/Container';
-import { Box } from './components/context/Box';
+import { Greet } from './components/props/Greet';
+import { Person } from './components/props/Person';
+import { PersonList } from './components/props/PersonList';
+import { Status } from './components/props/Status';
+import { Heading } from './components/props/Heading';
+import { Oscar } from './components/props/Oscar';
+import { Button } from './components/props/Button';
+import { Input } from './components/props/Input';
+import { Container } from './components/props/Container';
 import { ThemeContextProvider } from './components/context/ThemeContext';
-import { User } from './components/context/User';
+import { Box } from './components/context/Box';
 import { UserContextProvider } from './components/context/UserContext';
-import { Greet } from './components/Greet';
-import { Heading } from './components/Heading';
-import { Input } from './components/Input';
-import { Oscar } from './components/Oscar';
-import { Person } from './components/Person';
-import { PersonList } from './components/PersonList';
-import { Status } from './components/Status';
+import { User } from './components/context/User';
+// import { DomRef } from './components/refs/DomRef';
+// import { MutableRef } from './components/refs/MutableRef';
+import './App.css';
+import { Counter } from './components/class/Counter';
+import { List } from './components/generics/List';
+import { Toast } from './components/templateliterals/Toast';
+import { CustomButton } from './components/html/Button';
+import { RandomNumber } from './components/restriction/RandomNumber';
+import { Text } from './components/polymorphic/Text';
 
 function App() {
   const personName = {
     first: 'Bruce',
     last: 'Wayne',
   };
+
   const nameList = [
     {
       first: 'Bruce',
@@ -32,24 +41,72 @@ function App() {
       last: 'Diana',
     },
   ];
+
   return (
     <div className="App">
-      <Greet isLoggedIn={true} messageCount={10} name="Andrew" />
+      <Greet name="Vishwas" isLoggedIn={false} />
       <Person name={personName} />
       <PersonList names={nameList} />
-      <Status status="success" />
+      <Status status="loading" />
+      <Heading>Placeholder text</Heading>
       <Oscar>
-        <Heading>Oscar goes to London</Heading>
+        <Heading>Oscar goes to Dicaprio</Heading>
       </Oscar>
-      <Button handleClick={(e, id) => console.log('Button click', e, id)} />
-      <Input value="" handleChange={(e) => console.log(e)} />
-      <Container styles={{ border: '1px solid red', padding: '1rem' }} />
+      <Button
+        handleClick={(event, id) => {
+          console.log('Button clicked', event, id);
+        }}
+      />
+      <Input value="" handleChange={(event) => console.log(event)} />
+      <Container styles={{ border: '1px solid black', padding: '1rem' }} />
       <ThemeContextProvider>
         <Box />
       </ThemeContextProvider>
       <UserContextProvider>
         <User />
       </UserContextProvider>
+      {/* <DomRef />
+      <MutableRef /> */}
+      <Counter message="The count value is " />
+      {/* <List
+        items={['Batman', 'Superman', 'Wonder Woman']}
+        onClick={item => console.log(item)}
+      />
+      <List items={[1, 2, 3]} onClick={item => console.log(item)} /> */}
+      <List
+        items={[
+          {
+            id: 1,
+            first: 'Bruce',
+            last: 'Wayne',
+          },
+          {
+            id: 2,
+            first: 'Clark',
+            last: 'Kent',
+          },
+          {
+            id: 3,
+            first: 'Princess',
+            last: 'Diana',
+          },
+        ]}
+        onClick={(item) => console.log(item)}
+      />
+      <Toast position="center" />
+      <CustomButton variant="primary" onClick={() => console.log('Clicked')}>
+        Button Label
+      </CustomButton>
+      <RandomNumber value={10} isPositive />
+      <Text size="lg" as="h1">
+        Heading
+      </Text>
+      <Text size="md" as="p">
+        Paragraph
+      </Text>
+      <Text size="sm" color="secondary" as="label" htmlFor="someId">
+        Label
+      </Text>
     </div>
   );
 }
